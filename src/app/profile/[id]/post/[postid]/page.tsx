8bdf239d10c1bd6721732905d19ba7873e59437c
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-12-15 20:26:17 +0200

    updated postId page

--- src/app/profile/[id]/post/[postid]/page.tsx
+++ src/app/profile/[id]/post/[postid]/page.tsx
@@ -3,7 +3,9 @@ import { useParams } from "next/navigation"
 
 export default function ProfileIDPostID() {
     const params = useParams();
+    console.log("id")
     console.log(params.id);
+    console.log("postId");
     console.log(params.postId);
     return <div>My ID profile post page</div>
 }
