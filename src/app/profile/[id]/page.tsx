464d64409ec0239ff3c002dee6e36148125da6ba
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-12-15 20:11:25 +0200

    added useParams in id pages

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,3 +1,9 @@
+"use client"
+
+import { useParams } from "next/navigation";
+
 export default function ProfileID() {
+    const params = useParams();
+    console.log(params.id);
     return <div>My ID profile page</div>
 }
--- src/app/profile/[id]/post/[postid]/page.tsx
+++ src/app/profile/[id]/post/[postid]/page.tsx
@@ -1,3 +1,9 @@
+"use client"
+import { useParams } from "next/navigation"
+
 export default function ProfileIDPostID() {
+    const params = useParams();
+    console.log(params.id);
+    console.log(params.postId);
     return <div>My ID profile post page</div>
 }
