eded600f77450de05fb18e648dc4d950b7721da3
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-12-17 16:39:37 +0200

    updated progile page - prefetch

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,11 +10,22 @@ export default function Profile() {
     <div>
       <h1>My List of Profiles</h1>
       <div>
-        <ul className="cursor-pointer">
-          <Link href='/profile/1'>Profile 1</Link>
-          <Link href='/profile/2'>Profile 2</Link>
-          <Link href='profile/3'>Profile 3</Link>
-        </ul>
+        <div>
+          <Link href='/profile/1'>Profile 1 </Link>
+          <Link href='/profile/2'>Profile 2 </Link>
+          <Link
+           prefetch={false}
+           href={{
+            pathname: "/profile/3",
+            query: {
+              userName: "konvod",
+              userId: 12,
+              person: JSON.stringify({
+                age: 18,
+              }),
+            },
+          }}>Profile 3 </Link>
+        </div>
       </div>
     </div>
   );
