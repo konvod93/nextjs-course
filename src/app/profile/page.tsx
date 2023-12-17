2438d2da42ffc6962efe11e3246c35f1b25ba17d
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-12-17 16:09:17 +0200

    updated profile page- added Link component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Profile() {
   const router = useRouter();
-
-  console.log(router);
+  
   return (
     <div>
       <h1>My List of Profiles</h1>
       <div>
         <ul className="cursor-pointer">
-          <li onClick={() => router.push('profile/1')}>Profile 1</li>
-          <li onClick={() => router.push('profile/2')}>Profile 2</li>
-          <li onClick={() => router.push('profile/3')}>Profile 3</li>
+          <Link href='/profile/1'>Profile 1</Link>
+          <Link href='/profile/2'>Profile 2</Link>
+          <Link href='profile/3'>Profile 3</Link>
         </ul>
       </div>
     </div>
