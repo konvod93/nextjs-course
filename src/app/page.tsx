dde95aba9dbbd65f268d05bbf29224ce694aabab
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-12-19 19:55:45 +0200

    added imagge to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,23 @@
 
 import Header from "@/components/Header";
 import Logo from "@/components/Header/Logo";
+import Image from "next/image";
+import lhasa from "../../public/next.svg";
 
-export default function Home() {  
+export default function Home() {
   return (
-    <div>
-      <Header name="Konstantin" age={51} isMan={true}>
-        <Logo />
-      </Header>      
-      Main page of my application
-    </div>
-  )
+    <>
+      <div>
+        <Header name="Konstantin" age={51} isMan={true}>
+          <Logo />
+        </Header>
+        Main page of my application
+      </div>
+      <div>
+        <div className="w-[500px]">
+        <Image quality={50} src={lhasa} alt="lhasa" />
+        </div>
+      </div>
+    </>
+  );
 }
